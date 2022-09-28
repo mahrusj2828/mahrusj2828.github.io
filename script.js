deb8c716eee58db952619d0cc73f26e94644f762
mahrusj2828 <114589895+mahrusj2828@users.noreply.github.com>
2022-09-28 09:31:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Mahrus Jahan"; // update the part BETWEEN the "quotes"
+let userName = "mahrusj2828"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
